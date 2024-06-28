4100953ecb7bbea974aea5970795949ef71748ea
Eftychia Seisaki <eftychia.seisaki@eurodyn.com>
2024-06-28 20:39:17 +0300

    Create publisher entity and repo

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -16,6 +16,9 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
         inverseJoinColumns =  @JoinColumn(name = "author_id"))
@@ -58,6 +61,14 @@ public class Book {
 
     public Long getId() {
         return id;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     @Override
